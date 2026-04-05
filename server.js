fbbf1db32b2f50ee8dda82c70aedaef34fe62d74
PP Tree <pretaproduction@gmail.com>
2026-04-05 11:28:26 +0530

    fixed syntax error

--- server.js
+++ server.js
@@ -80,5 +80,5 @@ app.post("/webhook", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(🔥 Server running on port ${PORT});
+  console.log(`🔥 Server running on port ${PORT}`);
 });
